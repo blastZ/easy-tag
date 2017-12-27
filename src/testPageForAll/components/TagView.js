dcb469f6890da505b76f1b77e4779a83d383c438
blastz <touchzh1996@gmail.com>
2017-12-27 17:21:27 +0800

    Change tag view result style

--- src/testPageForAll/components/TagView.js
+++ src/testPageForAll/components/TagView.js
@@ -7,12 +7,46 @@ import { MenuItem } from 'material-ui/Menu';
 import Select from 'material-ui/Select';
 import { withStyles } from 'material-ui/styles';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import Chip from 'material-ui/Chip';
+
+const styles = {
+  paperRoot: {
+    width: '100%',
+    padding: '10px'
+  }
+}
 
 class TagView extends Component {
     state = {
     }
+    //
+    // <ul className="w3-ul w3-hoverable margin-top-5"  style={{overflowY: 'auto', flex: '1', padding: '0px 5px', marginTop: '10px !important'}}>{
+    //     this.props.boxList.map((box, index) => (
+    //         <li  className="w3-hover-green" key={box.x_start + box.y_end} style={{borderStyle: `${this.props.boxIndex === index ? 'dotted' : 'none'}`}}>
+    //             <div>
+    //                 <span>序号: {index + 1}</span>
+    //             </div>
+    //             <div>
+    //                 <div className="flex-box" style={{alignItems: 'center', padding: '5px 0px'}}>
+    //                     <span>标签: </span>
+    //                     <div className="flex-box" style={{overflowX: 'auto', marginLeft: '4px'}}>{
+    //                         box.tag.map((tag, index2) => (
+    //                             <div key={tag + index2} className="flex-box" style={{border: '2px solid black', alignItems: 'center', marginLeft: '2px', paddingLeft: '3px', paddingRight: '3px', whiteSpace: 'nowrap'}}>
+    //                                 {tag}
+    //                             </div>
+    //                         ))
+    //                     }</div>
+    //                 </div>
+    //             </div>
+    //             <div style={{display: 'flex', alignItems: 'center'}}>额外信息:<p style={{marginLeft: '5px'}}>{this.props.boxList[index].info}</p></div>
+    //         </li>
+    //     ))
+    // }</ul>
 
     render() {
+        const { classes } = this.props;
         return (
             <div className="flex-box flex-column" style={{justifyContent: 'center', height: '100%'}}>
                 <div>
@@ -35,31 +69,33 @@ class TagView extends Component {
                   </List>
                 </div>
                 <Divider />
-                <ul className="w3-ul w3-hoverable margin-top-5"  style={{overflowY: 'auto', flex: '1', padding: '0px 5px', marginTop: '10px !important'}}>{
-                    this.props.boxList.map((box, index) => (
-                        <li onClick={() => this.props.changeBoxIndex(index)} className="w3-hover-green" key={box.x_start + box.y_end} style={{borderStyle: `${this.props.boxIndex === index ? 'dotted' : 'none'}`}}>
-                            <div>
-                                <span>序号: {index + 1}</span>
-                            </div>
-                            <div>
-                                <div className="flex-box" style={{alignItems: 'center', padding: '5px 0px'}}>
-                                    <span>标签: </span>
-                                    <div className="flex-box" style={{overflowX: 'auto', marginLeft: '4px'}}>{
-                                        box.tag.map((tag, index2) => (
-                                            <div key={tag + index2} className="flex-box" style={{border: '2px solid black', alignItems: 'center', marginLeft: '2px', paddingLeft: '3px', paddingRight: '3px', whiteSpace: 'nowrap'}}>
-                                                {tag}
-                                            </div>
-                                        ))
-                                    }</div>
-                                </div>
-                            </div>
-                            <div style={{display: 'flex', alignItems: 'center'}}>额外信息:<p style={{marginLeft: '5px'}}>{this.props.boxList[index].info}</p></div>
-                        </li>
-                    ))
-                }</ul>
+                <List>
+                  {this.props.boxList.map((box, index) => (
+                    <ListItem key={index} button onClick={() => this.props.changeBoxIndex(index)} style={{background: `${this.props.boxIndex === index ? 'rgb(220,220,220)' : ''}`}}>
+                      <Paper classes={{
+                        root: classes.paperRoot
+                      }} elevation={4}>
+                        <Typography component="p">
+                          序号: {index + 1}
+                        </Typography>
+                        <div className="flex-box" style={{alignItems: 'center', padding: '5px 0px'}}>
+                          <span>标签: </span>
+                          <div className="flex-box" style={{overflowX: 'auto', marginLeft: '4px'}}>{
+                              box.tag.map((tag, index2) => (
+                                <Chip key={tag + index2} label={tag} />
+                              ))
+                          }</div>
+                        </div>
+                        <Typography component="p">
+                          额外信息: {this.props.boxList[index].info}
+                        </Typography>
+                      </Paper>
+                    </ListItem>
+                  ))}
+                </List>
             </div>
         )
     }
 }
 
-export default TagView
+export default withStyles(styles)(TagView);
